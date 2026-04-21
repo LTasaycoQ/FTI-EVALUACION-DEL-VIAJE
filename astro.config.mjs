e894b9a86597ef39f1d5dddcec1e3fb7c8cc6b7e
Luis Tasayco <luistasayco3030@gmail.com>
2026-04-21 02:25:03 +0000

    actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   base: "/",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "pr"],
+  },
   devToolbar: {
       enabled: false,
   },
