d93fc9b7bc4722082b5753e4c69f2db6d7df0985
Luis Tasayco <luistasayco3030@gmail.com>
2026-04-23 13:57:06 +0000

    actualiazcion

--- TE-PERU/astro.config.mjs
+++ TE-PERU/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/TE-PERU",
+  base: "/",
   build: {
     assets: 'assets', 
   },
