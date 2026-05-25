dfd8edcbde857dacfbd8aedfff10b5b5dbde2fef
LTasaycoQ <luistasayco3030@gmail.com>
2026-05-25 17:22:59 -0500

    actualizacion de configuradicon de formulario de evaluacion

--- TE-PERU/astro.config.mjs
+++ TE-PERU/astro.config.mjs
@@ -1,20 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
 export default defineConfig({
   base: "/",
   build: {
     assets: 'assets', 
   },
-  
+
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "pr"],
   },
+
   devToolbar: {
       enabled: false,
   },
+
   vite: {
   },
+
 });
